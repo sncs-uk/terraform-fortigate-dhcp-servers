cdd2653dba9b0ac10d855a1e63e6d4e74bd178e4
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-08-15 12:30:02 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
-# Example Terraform module
+<!-- BEGIN_TF_DOCS -->
+# Fortigate DHCP configuration module
 
-Terraform module which is an example.
-
-
-## Usage
-```hcl
-module "example" {
-  source        = "github.com/sncs-uk/example-module"
-  parameter     = "value"
-  do_something  = true
-}
-```
+This terraform module configures DHCP servers on a firewall
 
 ## Requirements
-| Name | Version |
-|------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0 |
-| <a name="requirement_example"></a> [example](#requirement\_example) | >= 1.23 |
+
+No requirements.
 
 ## Providers
 
 | Name | Version |
 |------|---------|
-| <a name="provider_example"></a> [example](#provider\_example) | >= 1.23 |
+| <a name="provider_fortios"></a> [fortios](#provider\_fortios) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [fortios_systemdhcp6_server.dhcp](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/systemdhcp6_server) | resource |
+| [fortios_systemdhcp_server.dhcp](https://registry.terraform.io/providers/fortinetdev/fortios/latest/docs/resources/systemdhcp_server) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_config_path"></a> [config\_path](#input\_config\_path) | Path to base configuration directory | `string` | n/a | yes |
+| <a name="input_vdoms"></a> [vdoms](#input\_vdoms) | List of VDOMs from which to pull in configuration | `list(string)` | `[]` | no |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,84 @@
+/**
+ * # Fortigate DHCP configuration module
+ *
+ * This terraform module configures DHCP servers on a firewall
+ */
+terraform {
+  required_providers {
+    fortios = {
+      source  = "fortinetdev/fortios"
+    }
+  }
+}
+
+locals {
+  dhcp_yaml       = yamldecode(file("${var.config_path}/config/dhcp-servers.yaml"))
+  dhcp            = try(local.dhcp_yaml.dhcp, [])
+  dhcp6           = try(local.dhcp_yaml.dhcp6, [])
+}
+locals {
+}
+
+resource fortios_systemdhcp_server dhcp {
+  for_each        = { for server in local.dhcp : server.interface => server}
+
+  interface       = each.value.interface
+  netmask         = each.value.netmask
+  default_gateway = each.value.gateway
+  lease_time      = each.value.lease_time
+
+  vdomparam       = each.value.vdom
+
+  dns_server1     = try(each.value.dns[0], null)
+  dns_server2     = try(each.value.dns[1], null)
+  dns_server3     = try(each.value.dns[2], null)
+  dns_server4     = try(each.value.dns[3], null)
+
+  ntp_server1     = try(each.value.ntp[0], null)
+  ntp_server2     = try(each.value.ntp[1], null)
+  ntp_server3     = try(each.value.ntp[2], null)
+
+  dynamic ip_range {
+    for_each    = { for range in each.value.ranges : range.start => range }
+    content {
+      id        = index(each.value.ranges, ip_range.value) + 1
+      start_ip  = ip_range.value.start
+      end_ip    = ip_range.value.end
+    }
+  }
+
+  dynamic reserved_address {
+    for_each    = { for reservation in try(each.value.reservations, []) : reservation.mac => reservation }
+    content {
+      id        = index(each.value.reservations, reserved_address.value) + 1
+      type      = "mac"
+      mac       = reserved_address.value.mac
+      ip        = reserved_address.value.ip
+      action    = "reserved"
+      description = reserved_address.value.description
+    }
+  }
+  dynamic_sort_subtable = "natural"
+}
+
+resource fortios_systemdhcp6_server dhcp {
+  for_each              = { for server in local.dhcp6 : server.interface => server}
+  depends_on            = [ module.interfaces ]
+
+  fosid                 = index(local.dhcp6, each.value) + 1
+  interface             = each.value.interface
+  subnet                = each.value.subnet
+  lease_time            = each.value.lease_time
+
+  ip_mode               = try(each.value.ip_mode, null)
+  delegated_prefix_iaid = try(each.value.delegated_prefix_iaid, null)
+  upstream_interface    = try(each.value.upstream_interface, null)
+
+  vdomparam             = each.value.vdom
+
+  dns_server1           = try(each.value.dns[0], null)
+  dns_server2           = try(each.value.dns[1], null)
+  dns_server3           = try(each.value.dns[2], null)
+  dns_server4           = try(each.value.dns[3], null)
+  dynamic_sort_subtable = "natural"
+}
