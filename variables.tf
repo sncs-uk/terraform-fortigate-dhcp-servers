4c82949052954f1686c2efc4d610a9f8423093cc
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-11-24 13:37:34 +0000

    Removed unused variable

--- variables.tf
+++ variables.tf
@@ -1,10 +1,5 @@
-variable vdoms {
-  description = "List of VDOMs from which to pull in configuration"
-  type        = list(string)
-  default     = []
-}
-
 variable config_path {
   description = "Path to base configuration directory"
   type        = string
 }
+
